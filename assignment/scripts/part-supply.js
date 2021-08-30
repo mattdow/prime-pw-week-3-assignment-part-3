8456c4d991f5b1ddb0803281dd10fdbba0b93466
Matt Dow <matt.dow.vo@gmail.com>
2021-08-30 15:57:45 -0500

    1st draft answers 8/30

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,41 +1,93 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40 
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+let partsNeeded = 40;
+console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+console.log('3. Second supplyChange is:', supplyChanges[1]);
 
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log('4. Removed item:', removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
+console.log('5. Adding 25 to supplyChanges.', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
-//    - if it is a positive number (greater than 0), log 'Added x parts.' 
+//    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
-//    - if the value is negative, format the log as 'Removed x parts.' 
+//    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
+for (let i = 0; i < supplyChanges.length; i++) {
+  if (supplyChanges[i] > 0) {
+    console.log('Added', supplyChanges[i], 'parts.');
+  }
+  else if (supplyChanges[i] === 0) {
+    console.log('No Change.');
+  }
+  else if (supplyChanges[i] < 0) {
+    console.log('Removed', -supplyChanges[i], 'parts.');
+  }
+}
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for (let supplyChange of supplyChanges) {
+  if (supplyChange > 0) {
+    console.log('Added', supplyChange, 'parts.');
+  }
+  else if (supplyChange === 0) {
+    console.log('No Change.');
+  }
+  else if (supplyChange < 0) {
+    console.log('Removed', -supplyChange, 'parts.');
+  }
+}
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+i = 0;
+while (i < supplyChanges.length) {
+  if (supplyChanges[i] > 0) {
+    console.log('Added', supplyChanges[i], 'parts.');
+  }
+  else if (supplyChanges[i] === 0) {
+    console.log('No Change.');
+  }
+  else if (supplyChanges[i] < 0) {
+    console.log('Removed', -supplyChanges[i], 'parts.');
+  }
+  i++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
+let totalSupplies = 0;
+for (let supplyChange of supplyChanges) {
+  totalSupplies += supplyChange;
+
+}
+console.log('9. Total supplies available is:', totalSupplies);
+
+// 10. Additional practice: I'll execute part 9 using the forEach command.
+
+console.log('10. Calculating the total with a forEach loop.');
+totalSupplies = 0;
+supplyChanges.forEach(supplyChange => {
+  totalSupplies += supplyChange;
+})
+
+console.log('Total supplies available is:', totalSupplies);
